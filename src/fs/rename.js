064ab4ab04c15b53effc1071fd51ea79db8f27e1
ddelena <89658366303@mail.ru>
2022-06-05 15:09:12 +0300

    rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,20 @@
+import fs from 'fs'
+import { fileURLToPath } from 'url'
+import path from 'path'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const rename = async () => {
-    // Write your code here 
+    
+    let prev = __dirname + '/files/wrongFilename.txt';
+    let curr = __dirname + '/files/properFilename.md'
+
+    if (!fs.existsSync(prev) || fs.existsSync(curr)) {
+        throw new Error('FS operation failed')
+    }
+    fs.rename(prev, curr, function(err) {
+        if ( err ) console.log('ERROR: ' + err)});
 };
+
+rename();
