3159d649922c74e7050f7d4572fa8b4fab875560
ddelena <89658366303@mail.ru>
2022-06-05 15:20:54 +0300

    delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,22 @@
+import fs from 'fs'
+import { fileURLToPath } from 'url'
+import path from 'path'
+import { deepStrictEqual } from 'assert';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const remove = async () => {
-    // Write your code here 
+    let fileToRemove = __dirname + '/files/fileToRemove.txt'
+
+    if (!fs.existsSync(fileToRemove)) {
+        throw new Error('FS operation failed')
+    }
+    fs.unlink(fileToRemove, (err) => {
+        if (err) {
+            throw err;
+        }
+    })
 };
+
+remove();
